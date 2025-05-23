c7b52200138a5ea1cfbb29272382d254a14dde95
unknown <laimecom123@gmail.com>
2025-05-22 22:49:57 -0500

    first version temp and wheater

---
+++ src/controllers/sensorT1.controllers.js
@@ -0,0 +1,43 @@
+const catchError = require('../utils/catchError');
+const SensorT1 = require('../models/SensorT1');
+
+const getAll = catchError(async(req, res) => {
+    const results = await SensorT1.findAll();
+    return res.json(results);
+});
+
+const create = catchError(async(req, res) => {
+    const result = await SensorT1.create(req.body);
+    return res.status(201).json(result);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await SensorT1.findByPk(id);
+    if(!result) return res.sendStatus(404);
+    return res.json(result);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await SensorT1.destroy({ where: {id} });
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await SensorT1.update(
+        req.body,
+        { where: {id}, returning: true }
+    );
+    if(result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const sensorT1Router = require('./sensorT1.router');
+
 const router = express.Router();
 
 // colocar las rutas aquí
 
+    router.use(sensorT1Router)
 
 module.exports = router;
