23c3b9080cbee6658568f1c8f4c971794aca8eec
unknown <laimecom123@gmail.com>
2025-05-22 23:17:12 -0500

    second commit not ssl

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false  // importante para aceptar certificados auto-firmados
+    }
+  }
+});
 
 module.exports = sequelize;
